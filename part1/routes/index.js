4ccc25b1e105a8375312ef68564c4a57b82733a4
michellenguyen1909 <michellehanng@gmail.com>
2025-06-21 06:25:23 +0000

    [automatic commit] 2025-06-21 06:25:23 UTC 122.108.4.134

---
+++ part1/routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+/* health check */
+router.get('/api', function(req, res, next) {
+  res.send('Backend runs successfully!');
+});
+
+module.exports = router;

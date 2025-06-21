799e2e12b9c31c6faf560169333b4f83dd155529
michellenguyen1909 <michellehanng@gmail.com>
2025-06-21 06:26:48 +0000

    [automatic commit] 2025-06-21 06:26:48 UTC 122.108.4.134

--- part1/app.js
+++ part1/app.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var path = require('path');
+const fs = require('fs');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mysql = require('mysql2/promise');

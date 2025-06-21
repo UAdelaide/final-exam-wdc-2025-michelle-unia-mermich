0c9b90139546f7d87620b26d0e8dd34ca5b37ea1
michellenguyen1909 <michellehanng@gmail.com>
2025-06-21 06:16:42 +0000

    [automatic commit] 2025-06-21 06:16:42 UTC 122.108.4.134

--- part1/app.js
+++ part1/app.js
@@ -26,7 +26,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
       console.log('Connected to DogWalkService');
 
-      
+      await db.execute(`
+      INSERT IGNORE INTO Users (username, email, password_hash, role) VALUES
+      ('alice123', 'alice@example.com', 'hashed123', 'owner'),
+      ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
+      ('carol123', 'carol@example.com', 'hashed789', 'owner')
+    `);
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);

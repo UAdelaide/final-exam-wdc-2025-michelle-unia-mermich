6d419a71bbd108708b6f8da28335800fb53b7fd9
michellenguyen1909 <michellehanng@gmail.com>
2025-06-21 07:10:13 +0000

    [automatic commit] 2025-06-21 07:10:13 UTC 122.108.4.134

--- part2/app.js
+++ part2/app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 require('dotenv').config();
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, '/public')));
+
+app.use(
+  session({
+    secret: 'dogwalking-secret',
+    resave: false,
+    saveUninitialized: false
+  })
+);
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
@@ -15,5 +25,5 @@ const userRoutes = require('./routes/userRoutes');
 app.use('/api/walks', walkRoutes);
 app.use('/api/users', userRoutes);
 
-// Export the app instead of listening here
+// Export app
 module.exports = app;
